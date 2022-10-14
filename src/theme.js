c03a548624f72bed000822a3cff8660cea94e2fb
Roger Costa <roger.desenv@gmail.com>
2022-10-14 15:01:22 -0300

    estrutura inicial + material-ui

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@material-ui/core/styles'
+
+
+// Create a theme instance.
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+    },
+});
+
+export default theme
